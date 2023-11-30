18a8449559a2bccd086077a72fe066c061c41052
PonmileDaniel <pomiledaniel@gmail.com>
2023-11-30 11:32:07 -0800

    number showe

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    nums = len(sys.argv)
+
+    if nums == 0:
+        print('{} arguments.'.format(nums - 1))
+    elif nums == 1:
+        print('{} arguments:'.format(nums - 1))
+    else:
+        print('{} arguments:'.format(nums - 1))
+        for n in range(1, nums):
+            print('{}: {}'.format(n, sys.argv[n]))
+             
