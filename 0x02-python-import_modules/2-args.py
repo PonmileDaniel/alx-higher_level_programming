a05196383407a6c061c41d246fd3ef26cbb8a76b
PonmileDaniel <pomiledaniel@gmail.com>
2023-11-30 11:40:31 -0800

    bbb

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     import sys
     nums = len(sys.argv)
-    if nums == 0:
+    if nums == 1:
         print('{} arguments.'.format(nums - 1))
-    elif nums == 1:
+    elif nums == 2:
         print('{} arguments:'.format(nums - 1))
     else:
         print('{} arguments:'.format(nums - 1))
