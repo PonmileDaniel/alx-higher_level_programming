4023ef5072754ab6f3b264dee996253cb8a4a5f6
PonmileDaniel <pomiledaniel@gmail.com>
2024-01-16 19:17:35 +0100

    trying to remodel

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for i in range(0, 90):
-    if i % 10 > i / 10 and i != 90:
-        
-        print("{:02d},".format(i), end=" ")
-print()
+    if i % 10 > i / 10:
+        if i != 89:
+            print("{:02d}, ".format(i), end="")
+        else:
+            print("{:02d}".format(i))
+    
