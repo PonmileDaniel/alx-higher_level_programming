73eb764ddb71938ea5e85373ef1b42ac4d21b798
PonmileDaniel <pomiledaniel@gmail.com>
2024-01-16 20:01:57 +0100

    reducing character

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 ldigit = abs(number) % 10
 if number < 0:
-    last_digit = -ldigit
-if last_digit > 5:
+    ldigit = -ldigit
+if ldigit > 5:
     print(f"Last digit of {number:d} is {ldigit} and is greater than 5")
-elif last_digit == 0:
+elif ldigit == 0:
     print(f"Last digit of {number:d} is {ldigit} and is 0")
 else:
     print(f"Last digit of {number:d} is {ldigit} and is less than 6 and not 0")
