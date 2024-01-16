8c4ce1b5e667685b1c4c64c95519bcea758697e2
PonmileDaniel <pomiledaniel@gmail.com>
2024-01-16 18:41:40 +0100

    more error

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 90):
     if i % 10 > i / 10 and i != 90:
-        print("{:02d},".format(i), end="")
-            
+        
+        print("{:02d},".format(i), end=" ")
+print()
