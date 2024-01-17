88ad77909c1617f6acba43ad11e1443f590899c6
PonmileDaniel <pomiledaniel@gmail.com>
2024-01-17 05:36:19 +0100

    adding space

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     for char in str:
         if 97 <= ord(char) <= 122:
             upper_case = chr(ord(char) - 32)
-            print("{}".format(upper_case), end="")
-        else:
-            print("{}".format(char), end="")
-    print()
+        print("{}".format(upper_case), end="")
+    print("")
+    
