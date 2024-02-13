72d4a2f80ef2d815f2207e37c88292f5eb638c44
PonmileDaniel <pomiledaniel@gmail.com>
2024-02-13 15:02:55 +0100

    Correcting whitespaces

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -4,22 +4,22 @@
 
 class Rectangle:
     """Represent a rectangle"""
-    
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle
-        
+
         Args:
             width (int): The width of the new rectangle
             height (int): THe height bof the new rectangle
         """
         self.width = width
         self.height = height
-        
+
     @property
     def width(self):
         """Get the width of the Rec"""
         return self.__width
-        
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -27,12 +27,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-            
+
     @property
     def height(self):
         """Get the height of the rectangle"""
         return self.__height
-        
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -40,11 +40,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        
+
     def area(self):
         """Return the area of the Rectangle."""
         return (self.__width * self.__height)
-        
+
     def perimeter(self):
         """Return the perimeter of the Rectangle"""
         if self.__width == 0 or self.__height == 0:
