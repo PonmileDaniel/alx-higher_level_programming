25003db2ef3bc280e06d2dd59abc2b051584280e
PonmileDaniel <pomiledaniel@gmail.com>
2024-02-20 14:47:05 +0100

    MAtrix ascending

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,6 +3,11 @@
 
 
 class MyList(list):
+    """subclass of list"""
+    def __init__(self):
+        """The object"""
+        super().__init__()
+
     def print_sorted(self):
         """Print a list sorted in ascending order"""
         sorted_list = sorted(self)
