a90fc6c01338f599950fd1646b662d165dfdafe9
PonmileDaniel <pomiledaniel@gmail.com>
2024-02-20 15:08:48 +0100

    MAtrix ascending

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,13 +3,14 @@
 
 
 def is_same_class(obj, a_class):
-    """Civen class.
+    """Check if an object is exactly an instance of a given class.
 
     Args:
-        obj : The object to check.
-        a_class : The class to match the type of obj to.
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
     Returns:
         If obj is exactly an instance of a_class - True.
+        Otherwise - False.
     """
     if type(obj) == a_class:
         return True
