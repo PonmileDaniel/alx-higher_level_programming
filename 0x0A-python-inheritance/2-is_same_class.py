cf75883553c1a1accb413f1658d9bd2601f1f4a7
PonmileDaniel <pomiledaniel@gmail.com>
2024-02-20 15:02:41 +0100

    MAtrix ascending

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,10 @@
 
 
 def is_same_class(obj, a_class):
-    """"""
+    """
+    obj: The object to check
+    a_class: The class to compare against.
+    """
     if type(obj) == a_class:
         return True
     return False
