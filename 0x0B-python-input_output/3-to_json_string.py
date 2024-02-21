f1e2b0524dc0b164af73e88239b3964ed42ad081
PonmileDaniel <pomiledaniel@gmail.com>
2024-02-21 16:21:03 +0100

    json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""Defines a json"""
+"""Defines a string-to-JSON function."""
 import json
 
 
 def to_json_string(my_obj):
-    """Return the json
-    Args:
-        my_obj: object
-        
-    """
+    """Return the JSON  of a string object."""
     return json.dumps(my_obj)
