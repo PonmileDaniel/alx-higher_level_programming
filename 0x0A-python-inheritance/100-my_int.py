b61e2c03ff90d6d94fcbc849c49033b25d5c041e
PonmileDaniel <pomiledaniel@gmail.com>
2024-02-21 12:43:18 +0100

    override operator

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Override operator"""
+
+
 class MyInt(int):
     """ Class that inherits from class int"""
-
-
     def __eq__(self, other):
         """ Method that returns != check """
         return int.__ne__(self, other)
