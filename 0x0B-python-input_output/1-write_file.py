5af3496525eedf96530dbb019b5c8a6472b275e5
PonmileDaniel <pomiledaniel@gmail.com>
2024-02-21 15:30:30 +0100

    Define a function

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,7 @@
 def write_file(filename="", text=""):
     """Write a string to utf-8"""
     n = 0
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         for line in f:
             n += 1
     return f.write(n)
