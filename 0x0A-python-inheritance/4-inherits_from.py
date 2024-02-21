46bdd47516dd6a733b6513670ad94c1a0e980a0b
PonmileDaniel <pomiledaniel@gmail.com>
2024-02-21 11:58:33 +0100

    inherited a class

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""The object of an instance of a class that inherit direct or indirectly"""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is inherited of a class"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
