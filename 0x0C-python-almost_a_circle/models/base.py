c1e49a591c389d1f26eb181f7092af03f4e0915b
PonmileDaniel <pomiledaniel@gmail.com>
2024-04-23 14:13:47 +0100

    Remove newline

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''For base class'''
+
+
+class Base:
+    '''Representation of the base of the oop'''
+    
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        '''Construct'''
+        if id is None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
