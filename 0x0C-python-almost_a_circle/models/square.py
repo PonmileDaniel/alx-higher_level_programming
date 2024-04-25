798c1dc519b414d195b6ece0828562bea3e376bd
PonmileDaniel <pomiledaniel@gmail.com>
2024-04-25 20:41:07 +0100

    Removing whitespaces

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -41,9 +41,8 @@ class Square(Rectangle):
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
-     
+
     def to_dictionary(self):
         '''Return Dictionary representation of a rectangle'''
         return {"id": self.id, "size": self.size,
                 "x": self.x, "y": self.y}
-
