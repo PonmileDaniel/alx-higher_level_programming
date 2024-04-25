95ed4bd2b70509d62c532463c419dd2bdd7eadfa
PonmileDaniel <pomiledaniel@gmail.com>
2024-04-25 16:21:15 +0100

    whitespaces

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,11 +5,11 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     '''A square Class'''
-    
+
     def __init__(self, size, x=0, y=0, id=None):
         '''Constructor'''
         super().__init__(size, size, x, y, id)
-        
+
     def __str__(self):
         return f"[Square] ({(self.id)}) {self.x}/{self.y} - " \
             f"{self.width}"
