eeb0fd1e8ca711dd62ad8e28bfb729724a31da0f
PonmileDaniel <pomiledaniel@gmail.com>
2024-04-25 20:49:50 +0100

    Json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''For base class'''
+from json import dumps, loads
 
 
 class Base:
@@ -14,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+            
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''Json return'''
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        else:
+            return dumps(list_dictionaries)
