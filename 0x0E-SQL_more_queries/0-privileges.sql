a724c47745185829f17ec03aae5a51db0bb4c78b
PonmileDaniel <pomiledaniel@gmail.com>
2024-05-24 16:01:07 +0100

    Sql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- Script that list all priviledges of the Mysql users
+-- Query to list all priviledge (GRANT) of the Mysql
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
