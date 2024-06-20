1a6c2e197481a5c7e0c38bcdf353e5792223afc3
PonmileDaniel <pomiledaniel@gmail.com>
2024-06-20 11:45:44 +0100

    more on loops

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,6 @@ if (isNaN(number)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < number; i++) {
-    console.log('c is fun');
+    console.log('C is fun');
   }
 }
