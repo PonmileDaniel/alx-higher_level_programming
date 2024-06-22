2044584741773f1f4f86a41f90a3a49cd952ff6b
PonmileDaniel <pomiledaniel@gmail.com>
2024-06-22 03:39:11 +0100

    concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,18 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const source1 = fs.readFileSync(process.argv[2]).toString();
-const source2 = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], source1 + source2);
+try {
+  // Read the contents of the first source file
+  const source1 = fs.readFileSync(process.argv[2], 'utf8');
+
+  // Read the contents of the second source file
+  const source2 = fs.readFileSync(process.argv[3], 'utf8');
+
+  // Write the concatenated contents to the destination file
+  fs.writeFileSync(process.argv[4], source1 + source2);
+
+  console.log(`Successfully concatenated ${process.argv[2]} and ${process.argv[3]} into ${process.argv[4]}`);
+} catch (err) {
+  console.error('Error:', err.message);
+  process.exit(1);
+}
