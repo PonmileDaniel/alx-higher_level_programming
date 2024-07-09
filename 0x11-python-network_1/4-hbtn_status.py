342211045658844f12846398fda7f5d1402cf774
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-09 09:16:52 +0100

    4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,4 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     body = response.content
-    print('Body response:')
-    print('\t- type: {}'.format(type(body)))
-    print('\t- content: {}'.format(body))
-    print('\t- utf8 content: {}'.format(body.decode("utf-8")))
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(body), body))
