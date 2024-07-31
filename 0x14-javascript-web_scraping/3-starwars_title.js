cd2bc8f5e049530cb08074dbe8bf5ff4284aa31b
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-31 22:01:06 +0100

    Api link

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const ids = process.argv[2];
-request('http://swapi.co/api/films/' + ids + '/', function (error, response, body) {
+request('http://swapi-api.alx-tools.com/api/films/' + ids + '/', function (error, response, body) {
   if (error == null) {
     const json = JSON.parse(body);
     console.log(json.title);
