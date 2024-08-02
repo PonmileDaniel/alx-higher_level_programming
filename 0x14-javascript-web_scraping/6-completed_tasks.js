c0e9c1691ba84e55e07cc0665e4e76aeef12c262
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-02 02:41:01 +0100

    TAsk 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error === null) {
+    const taskCountByUser = {};
+    const tasks = JSON.parse(body);
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].completed === true) {
+        if (taskCountByUser[tasks[i].userId] === undefined) {
+          taskCountByUser[tasks[i].userId] = 0;
+        }
+        taskCountByUser[tasks[i].userId]++;
+      }
+    }
+    console.log(taskCountByUser);
+  } else {
+    console.error('Error:', error);
+  }
+});
