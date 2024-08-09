c2ee382a893d7363d8b0c0693d597c9910db677d
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-09 05:36:52 +0100

    Commited most task at once

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+const $ = window.$;
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  const movies = data.results;
+  movies.forEach(movie => {
+    $('#list_movies').append(`<li>${movie.title}</li>`);
+  });
+});
